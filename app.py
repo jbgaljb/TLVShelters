380dc4151b44d08014955a9e99c20ab3f9f9a9ea
galregev5 <galregev5@gmail.com>
2024-09-12 16:27:43 +0300

    mvp4

--- app.py
+++ app.py
@@ -9,15 +9,10 @@ def index():
     # Define the path to your CSV file
     file_path = os.path.join(os.path.dirname(__file__), 'sheltersCSV.csv')
     
-    # Load the CSV file into a pandas DataFrame
+    # Load the CSV file into a pandas DataFrame, and then to json which is served
     df = pd.read_csv(file_path)
     locations = df.to_json
-    print(locations)
-
-    # Display the DataFrame
-    # print(locations)
-    # return render_template('index.html', locations = locations)
-    print("hello world")
+    return locations
 
 if __name__ == '__main__':
     app.run()
