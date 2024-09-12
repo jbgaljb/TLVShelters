53e66b778f87ed31b9d8cb361658cc787bb29506
galregev5 <galregev5@gmail.com>
2024-09-12 17:16:08 +0300

    mvp5

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
 import pandas as pd
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+# Enable CORS for all routes
+CORS(app)
+
 @app.route('/')
 def index():
-    # Define the path to your CSV file
+    # Define the path to your CSV file (commented out - local context)
     file_path = os.path.join(os.path.dirname(__file__), 'sheltersCSV.csv')
+    # file_path = os.path.join(r'C:\Users\galre\Documents\jhonBrice\sheltersProj\backend', 'sheltersCSV.csv')
     
     # Load the CSV file into a pandas DataFrame, and then to json which is served
     df = pd.read_csv(file_path)
-    locations = df.to_json
+    locations = df.to_json(orient='records')
     return locations
 
 if __name__ == '__main__':
